e21906fa2ebb36bafd0c1c8b2e2d47aeca97e5c3
Conrad Parker <conrad@metadecks.org>
2006-05-30 20:55:02 +0800

    count pages
    count the total number of Ogg pages in the input
    
    darcs-hash:20060530125502-4b390-4888254909207cbcc7276b44981763fdc9b2ad58.gz

--- ogg.hs
+++ ogg.hs
@@ -1,17 +1,17 @@
 module Main where
 
-_scan :: [Char] -> [Char] -> [Char] -> Bool
-_scan _ [] _ = True
-_scan _ _ [] = False
-_scan r (s:ss) (t:ts)
-    | s == t	= _scan (r++[s]) ss ts
-    | otherwise	= _scan [] (r++[s]++ss) ts
+scancnt :: Int -> [Char] -> [Char] -> [Char] -> Int
+scancnt x r [] t = scancnt (x+1) [] r t
+scancnt x _ _ [] = x
+scancnt x r (s:ss) (t:ts)
+    | s == t	= scancnt x (r++[s]) ss ts
+    | otherwise	= scancnt x [] (r++[s]++ss) ts
 
-scan = _scan []
+scan = scancnt 0 []
 
 pagescan = scan "OggS"
 
 main :: IO ()
 main = do input <- getContents
-          putStrLn (show (pagescan input))
+          putStrLn ((show (pagescan input)) ++ " pages")
 
