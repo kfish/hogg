20d8d1848fb3aea3a348662a78e72455b849ba71
Conrad Parker <conrad@metadecks.org>
2006-06-04 19:19:34 +0800

    add separate hogg-pagedump tool
    darcs-hash:20060604111934-4b390-64a329750730da936f4dedd30396623af0421519.gz

--- hogg/tools/hogg-pagedump.hs
+++ hogg/tools/hogg-pagedump.hs
@@ -5,5 +5,5 @@ import Ogg.Demux
 
 main :: IO ()
 main = do input <- L.getContents
-          putStrLn (show (pages2packets (map readPage (pageSplit $ L.unpack input))))
+          putStrLn (show (map readPage (pageSplit $ L.unpack input)))
 
