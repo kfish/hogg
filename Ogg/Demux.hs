537050d213f8f5f2a2c435d931d645752e3bb6bc
Conrad Parker <conrad@metadecks.org>
2006-06-06 03:40:50 +0800

    move hexdump to its own module
    darcs-hash:20060605194050-4b390-d6b64a2d3e69d2d10177f1a1f9d725a1dd51c53b.gz

--- Ogg/Demux.hs
+++ Ogg/Demux.hs
@@ -9,10 +9,10 @@
 module Ogg.Demux where
 
 import Ogg.Utils
+import Ogg.Dump
 
 import Data.Word (Word8)
 import Data.Bits
-import Data.Char (isSpace, chr, ord)
 
 import Text.Printf
 
@@ -56,37 +56,6 @@ data OggPacket =
 instance Show Granulepos where
   show (Granulepos (Nothing)) = "-1"
   show (Granulepos (Just gp)) = show gp
-
-------------------------------------------------------------
--- Dump
---
-
-hexDump :: [Word8] -> String
-hexDump = _hexDump 0 ""
-
-_hexDump :: Int -> String -> [Word8] -> String
-_hexDump _ s [] = s
-_hexDump o s d = _hexDump (o+16) (s++lineDump) rest
-    where (line, rest) = splitAt 16 d
-          lineDump = spaces 4 ++ offset ++ ": " ++ hexLine ++ spaces hexPad ++ ascLine ++ "\n"
-          spaces n = take n $ repeat ' '
-          offset = printf "%04x" o
-
-          hexLine = hexSpace "" hexList False
-          hexPad = 1 + 8*5 - length hexLine
-          hexList = map hexByte line
-          hexByte x = printf "%02x" ((fromIntegral x)::Int)
-          hexSpace x [] _ = x
-          hexSpace x (c:cs) True = hexSpace (x++c++" ") cs False
-          hexSpace x (c:cs) False = hexSpace (x++c) cs True
-
-          ascLine = concat $ map ascByte chars
-          chars = map chr (map fromIntegral line)
-          ascByte c
-            | (ord c) > 126 = "."
-            | isSpace c = " "
-            | (ord c) < 32 = "."
-            | otherwise = [c]
 
 ------------------------------------------------------------
 -- OggPage functions
---
+++ Ogg/Dump.hs
@@ -0,0 +1,46 @@
+--
+-- Module      : Dump
+-- Copyright   : (c) Conrad Parker 2006
+-- License     : BSD-style
+-- Maintainer  : conradp@cse.unsw.edu.au
+-- Stability   : experimental
+-- Portability : portable
+
+module Ogg.Dump where
+
+import Data.Word (Word8)
+import Data.Char (isSpace, chr, ord)
+
+import Text.Printf
+
+------------------------------------------------------------
+-- Dump
+--
+
+hexDump :: [Word8] -> String
+hexDump = _hexDump 0 ""
+
+_hexDump :: Int -> String -> [Word8] -> String
+_hexDump _ s [] = s
+_hexDump o s d = _hexDump (o+16) (s++lineDump) rest
+    where (line, rest) = splitAt 16 d
+          lineDump = spaces 4 ++ offset ++ ": " ++ hexLine ++ spaces hexPad ++ ascLine ++ "\n"
+          spaces n = take n $ repeat ' '
+          offset = printf "%04x" o
+
+          hexLine = hexSpace "" hexList False
+          hexPad = 1 + 8*5 - length hexLine
+          hexList = map hexByte line
+          hexByte x = printf "%02x" ((fromIntegral x)::Int)
+          hexSpace x [] _ = x
+          hexSpace x (c:cs) True = hexSpace (x++c++" ") cs False
+          hexSpace x (c:cs) False = hexSpace (x++c) cs True
+
+          ascLine = concat $ map ascByte chars
+          chars = map chr (map fromIntegral line)
+          ascByte c
+            | (ord c) > 126 = "."
+            | isSpace c = " "
+            | (ord c) < 32 = "."
+            | otherwise = [c]
+
