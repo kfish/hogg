e41bafdabd5a8a6be02b8c86f2fcbb340d663efe
Conrad Parker <conrad@metadecks.org>
2006-06-06 20:49:01 +0800

    take filename from commandline
    darcs-hash:20060606124901-4b390-012069a04c4bc6726c7a960444f6ea76125c3bf3.gz

--- tools/hogg.hs
+++ tools/hogg.hs
@@ -1,32 +1,42 @@
 module Main where
 
+import Control.Monad
+
 import System.Environment
+import System.IO
 import System.Console.GetOpt
 
 import qualified Data.ByteString.Lazy as L
 import Ogg.Page
 import Ogg.Packet
 
-dumpPackets :: IO ()
-dumpPackets = do
-    input <- L.getContents
+dumpPackets :: String -> IO ()
+dumpPackets filename = do
+    handle <- openFile filename ReadMode
+    input <- L.hGetContents handle
     mapM_ putStrLn (map show (pages2packets (pageScan $ L.unpack input)))
 
-countPages :: IO ()
-countPages = do
-    input <- L.getContents
+countPages :: String -> IO ()
+countPages filename = do
+    handle <- openFile filename ReadMode
+    input <- L.hGetContents handle
     putStrLn $ (show $ length (pageScan $ L.unpack input)) ++ " pages"
 
-dumpPages :: IO ()
-dumpPages = do
-    input <- L.getContents
+dumpPages :: String -> IO ()
+dumpPages filename = do
+    handle <- openFile filename ReadMode
+    input <- L.hGetContents handle
     mapM_ putStrLn (map show (pageScan $ L.unpack input))
+
+getFilename :: [String] -> IO String
+getFilename args = return $ last args
 
 main :: IO ()
 main = do
     (command:args) <- getArgs
+    filename <- getFilename args
     case command of
-      "dump" -> dumpPackets
-      "pagecount" -> countPages
-      "pagedump" -> dumpPages
+      "dump" -> dumpPackets filename
+      "pagecount" -> countPages filename
+      "pagedump" -> dumpPages filename
 
