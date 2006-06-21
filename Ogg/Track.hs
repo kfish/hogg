a1c108f12b5bbb92a65939336e38bb7a9fe1fc84
Conrad Parker <conrad@metadecks.org>
2006-06-21 16:13:24 +0800

    add identification of speex and theora
    darcs-hash:20060621081324-4b390-7c1d25199162b04e2da49e5b4883c55c0bd8b7f9.gz

--- Ogg/Track.hs
+++ Ogg/Track.hs
@@ -37,9 +37,17 @@ nullTrack = OggTrack 0 Nothing
 vorbisIdent :: [Word8]
 vorbisIdent = [0x01, 0x76, 0x6f, 0x72, 0x62, 0x69, 0x73]
 
+theoraIdent :: [Word8]
+theoraIdent = [0x80, 0x74, 0x68, 0x65, 0x6f, 0x72, 0x61]
+
+speexIdent :: [Word8]
+speexIdent = [0x53, 0x70, 0x65, 0x65, 0x78, 0x20, 0x20, 0x20]
+
 readCType :: [Word8] -> Maybe OggType
-readCType (r1:r2:r3:r4:r5:r6:r7:_)
+readCType (r1:r2:r3:r4:r5:r6:r7:r8:_)
   | [r1,r2,r3,r4,r5,r6,r7] == vorbisIdent = Just Vorbis
+  | [r1,r2,r3,r4,r5,r6,r7,r8] == speexIdent = Just Speex
+  | [r1,r2,r3,r4,r5,r6,r7] == theoraIdent = Just Theora
   | otherwise = Nothing
 readCType _ = Nothing
 
