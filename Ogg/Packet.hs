8e31271a3dadfbcbfca8bb625730e315b2f6d5ac
Conrad Parker <conrad@metadecks.org>
2006-07-27 15:29:21 +0800

    retain page order for reconstruction
    When building packets, retain the page order in the segmentation information.
    Then, when reconstructing pages, maintain a temporary queue of completed pages
    and dequeue only pending pages (ie. maintain an expected page index, and
    dequeue when the page with that index is on the head of the tmp queue).
    
    darcs-hash:20060727072921-4b390-79dd47a329e0f18a9007f39c83e44df80b1da31c.gz

--- Ogg/Packet.hs
+++ Ogg/Packet.hs
@@ -39,6 +39,7 @@ data OggPacket =
 data OggSegment =
   OggSegment {
     segmentLength :: Int,
+    segmentPageIx :: Int, -- ^ page index (NOT seqno) of this segment
     segmentEndsPage :: Bool -- ^ whether or not the segment ends a page
   }
 
@@ -55,75 +56,100 @@ packetIsType t p = trackIsType t (packetTrack p)
 
 -- A map from track serialno to seqno
 type SeqnoMap = Map.Map OggTrack Word32
-type CarryPages = Map.Map OggTrack OggPage
+
+data CarryPage = CarryPage {
+  carryPageIx :: Int,
+  carryPagePage :: OggPage
+}
+
+instance Eq CarryPage where
+  (==) (CarryPage ix1 _) (CarryPage ix2 _) = (==) ix1 ix2
+
+instance Ord CarryPage where
+  compare (CarryPage ix1 _) (CarryPage ix2 _) = compare ix1 ix2
+
+type CarryPages = Map.Map OggTrack CarryPage
 
 packetsToPages :: [OggPacket] -> [OggPage]
-packetsToPages = packetsToPages_ Map.empty Map.empty
+packetsToPages = packetsToPages_ Map.empty Map.empty 0 []
 
-packetsToPages_ :: CarryPages -> SeqnoMap -> [OggPacket] -> [OggPage]
+packetsToPages_ :: CarryPages -> SeqnoMap -> Int -> [CarryPage] -> [OggPacket] -> [OggPage]
 
-packetsToPages_ carry _ [] = elems carry
+packetsToPages_ carry _ _ q [] = List.map carryPagePage (q ++ (elems carry))
 
-packetsToPages_ carry sqMap (p:ps)
-  = newPages ++ packetsToPages_ newCarry newSqMap ps
+packetsToPages_ carry sqMap ix pageQueue (p:ps)
+  = newPages ++ packetsToPages_ newCarry newSqMap newIx newQueue ps
   where
-    (newPages, newCarry) = segsToPages [] carry False seqno p
-    n = fromIntegral (length newPages)
-    track = packetTrack p
-    seqno = Map.findWithDefault 0 track sqMap
-    newSqMap = Map.insert track (seqno+n) sqMap
+    (newIx, newPages, newQueue) = dequeuePages ix [] tmpQueue
+    (tmpQueue, newCarry, newSqMap) = segsToPages pageQueue carry False sqMap p
+
+dequeuePages :: Int -> [OggPage] -> [CarryPage] -> (Int, [OggPage], [CarryPage])
+dequeuePages ix oldPages [] = (ix, oldPages, [])
+dequeuePages ix oldPages oldQueue@((CarryPage qix qg):qs)
+  | ix == qix = dequeuePages (ix+1) (oldPages++[qg]) qs
+  | otherwise = (ix, oldPages, oldQueue)
 
 -- | Convert segments of a packet into pages, and maybe a carry page
-segsToPages :: [OggPage] -> CarryPages -> Bool -> Word32 -> OggPacket
-               -> ([OggPage], CarryPages)
+segsToPages :: [CarryPage] -> CarryPages -> Bool -> SeqnoMap -> OggPacket
+               -> ([CarryPage], CarryPages, SeqnoMap)
 
-segsToPages pages carry _ _ (OggPacket _ _ _ _ _ Nothing) = (pages, carry)
-segsToPages pages carry _ _ (OggPacket _ _ _ _ _ (Just [])) = (pages, carry)
+segsToPages pages carry _ sqMap (OggPacket _ _ _ _ _ Nothing) =
+  (pages, carry, sqMap)
 
-segsToPages pages carry cont seqno p@(OggPacket _ track _ _ _ (Just [s]))
-  | segmentEndsPage s = (pages++[newPage], deleteCarry)
-  | otherwise         = (pages, replaceCarry)
+segsToPages pages carry _ sqMap (OggPacket _ _ _ _ _ (Just [])) =
+  (pages, carry, sqMap)
+
+segsToPages pages carry cont sqMap p@(OggPacket _ track _ _ _ (Just [s]))
+  | segmentEndsPage s = (newPages, deleteCarry, newSqMap)
+  | otherwise         = (pages, replaceCarry, sqMap)
   where
-    newPage = appendToCarry carryPage cont seqno p
+    newPages = List.insert newPage pages
+    newPage = appendToCarry carryPage (segmentPageIx s) cont seqno p
+    seqno = Map.findWithDefault 0 track sqMap
+    newSqMap = Map.insert track (seqno+1) sqMap
     carryPage = Map.lookup track carry
     deleteCarry = Map.delete track carry
     replaceCarry = Map.insert track newPage carry
 
-segsToPages pages carry cont seqno
+segsToPages pages carry cont sqMap
             p@(OggPacket d track gp _ eos (Just (s:ss)))
-  = segsToPages (pages++[newPage]) deleteCarry True (seqno+1) dropPacket
+  = segsToPages newPages deleteCarry True newSqMap dropPacket
   where
+    newPages = List.insert newPage pages
     dropPacket = OggPacket rest track gp False eos (Just ss)
     rest = drop (segmentLength s) d
+    seqno = Map.findWithDefault 0 track sqMap
+    newSqMap = Map.insert track (seqno+1) sqMap
     deleteCarry = Map.delete track carry
-    newPage = appendToCarry carryPage cont seqno p
+    newPage = appendToCarry carryPage (segmentPageIx s) cont seqno p
     carryPage = Map.lookup track carry
 
 -- | Append the first segment of a packet to (maybe) a carry page
-appendToCarry :: Maybe OggPage -> Bool -> Word32 -> OggPacket -> OggPage
+appendToCarry :: Maybe CarryPage -> Int -> Bool -> Word32 -> OggPacket -> CarryPage
 
 -- Case of no carry page, packet has only one segment
-appendToCarry Nothing cont seqno (OggPacket d track gp bos eos (Just [_]))
-  = OggPage 0 track cont True bos eos gp seqno [d]
+appendToCarry Nothing ix cont seqno (OggPacket d track gp bos eos (Just [_]))
+  = CarryPage ix (OggPage 0 track cont True bos eos gp seqno [d])
 
 -- Case of no carry page, packet has >1 segment
-appendToCarry Nothing cont seqno (OggPacket d track _ bos _ (Just (s:_)))
-  = OggPage 0 track cont False bos False (Granulepos Nothing) seqno [seg]
-  where seg = take (segmentLength s) d
+appendToCarry Nothing ix cont seqno (OggPacket d track _ bos _ (Just (s:_)))
+  = CarryPage ix (OggPage 0 track cont False bos False (Granulepos Nothing) seqno [seg])
+  where
+    seg = take (segmentLength s) d
 
 -- Case of a carry page, packet has only one segment
-appendToCarry (Just (OggPage o track cont _ bos _ _ seqno segs)) _ _
+appendToCarry (Just (CarryPage ix (OggPage o track cont _ bos _ _ seqno segs))) _ _ _
               (OggPacket d _ gp _ eos (Just [_]))
-  = OggPage o track cont True bos eos gp seqno (segs++[d])
+  = CarryPage ix (OggPage o track cont True bos eos gp seqno (segs++[d]))
 
 -- Case of a carry page, packet has >1 segment
-appendToCarry (Just (OggPage o track cont _ bos _ gp seqno segs)) _ _
+appendToCarry (Just (CarryPage ix (OggPage o track cont _ bos _ gp seqno segs))) _ _ _
               (OggPacket d _ _ _ eos (Just (s:_)))
-  = OggPage o track cont False bos eos gp seqno (segs++[seg])
+  = CarryPage ix (OggPage o track cont False bos eos gp seqno (segs++[seg]))
   where seg = take (segmentLength s) d
         
 -- For completeness
-appendToCarry _ _ _ _ = error "appendToCarry{Ogg.Packet}: nothing to append"
+appendToCarry _ _ _ _ _ = error "appendToCarry{Ogg.Packet}: nothing to append"
 
 ------------------------------------------------------------
 -- pagesToPackets
@@ -132,32 +158,32 @@ appendToCarry _ _ _ _ = error "appendToCarry{Ogg.Packet}: nothing to append"
 type CarryPackets = Map.Map OggTrack OggPacket
 
 pagesToPackets :: [OggPage] -> [OggPacket]
-pagesToPackets = _pagesToPackets Map.empty
+pagesToPackets = _pagesToPackets Map.empty 0
 
-_pagesToPackets :: CarryPackets -> [OggPage] -> [OggPacket]
-_pagesToPackets carry [] = elems carry
-_pagesToPackets carry [g] = prependCarry carry (pageToPackets g)
+_pagesToPackets :: CarryPackets -> Int -> [OggPage] -> [OggPacket]
+_pagesToPackets carry _ [] = elems carry
+_pagesToPackets carry ix [g] = prependCarry carry (pageToPackets ix g)
 
-_pagesToPackets carry (g:gs) =
+_pagesToPackets carry ix (g:gs) =
     if (incplt && length ps == 1) then
-        _pagesToPackets (carryCarry carry newcarry) gs
+        _pagesToPackets (carryCarry carry newcarry) (ix+1) gs
     else
-        s ++ _pagesToPackets newcarry gs
+        s ++ _pagesToPackets newcarry (ix+1) gs
     where s = prependCarry carry ns
           newcarry = if incplt then Map.insert track (last ps) carry
                                else Map.delete track carry
           track = pageTrack g
           ns = if incplt then init ps else ps
-          ps = pageToPackets g
+          ps = pageToPackets ix g
           incplt = pageIncomplete g
 
 -- | Construct (partial) packets from the segments of a page
-pageToPackets :: OggPage -> [OggPacket]
-pageToPackets page = setLastSegmentEnds p3
+pageToPackets :: Int -> OggPage -> [OggPacket]
+pageToPackets ix page = setLastSegmentEnds p3
     where p3 = setGranulepos p2 (pageGranulepos page) (pageIncomplete page)
           p2 = setEOS p1 (pageEOS page)
           p1 = setBOS p0 (pageBOS page)
-          p0 = List.map (packetBuild (pageTrack page)) (pageSegments page)
+          p0 = List.map (packetBuild (pageTrack page) ix) (pageSegments page)
 
 setLastSegmentEnds :: [OggPacket] -> [OggPacket]
 setLastSegmentEnds [] = []
@@ -185,10 +211,10 @@ setEOS [] _ = []
 setEOS ps False = ps
 setEOS ps True = (init ps)++[(last ps){packetEOS = True}]
 
--- | Build a partial packet given a track and a segment
-packetBuild :: OggTrack -> [Word8] -> OggPacket
-packetBuild track r = OggPacket r track (Granulepos Nothing) False False (Just [seg])
-    where seg = OggSegment l False
+-- | Build a partial packet given a track, seqno and a segment
+packetBuild :: OggTrack -> Int -> [Word8] -> OggPacket
+packetBuild track ix r = OggPacket r track (Granulepos Nothing) False False (Just [seg])
+    where seg = OggSegment l ix False
           l = length r
 
 -- | Concatenate data of two (partial) packets into one (partial) packet
