cf634277ae17eb1446955f08ecf542f448e83d4b
Conrad Parker <conrad@metadecks.org>
2006-08-11 13:10:42 +0800

    add basic Ogg Skeleton support: recognize and filtering on Skeleton ident
    Ogg Skeleton (http://wiki.xiph.org/index.php/OggSkeleton) provides some
    extra stream info. Recognize the bos header and allow filtering on skeleton
    track (for hogg dump etc.)
    
    darcs-hash:20060811051042-4b390-8de02b5ffe73bf2ac9acbf80c0c3826473aa0d9b.gz

--- Ogg/Track.hs
+++ Ogg/Track.hs
@@ -22,7 +22,7 @@ import qualified Data.ByteString.Lazy as L
 -- Data
 --
 
-data OggType = Vorbis | Speex | Theora
+data OggType = Skeleton | Vorbis | Speex | Theora
   deriving Eq
 
 data OggTrack =
@@ -44,6 +44,10 @@ trackIsType _ _ = False
 nullTrack :: OggTrack
 nullTrack = OggTrack 0 Nothing
 
+-- skeletonIdent = 'fishead\0'
+skeletonIdent :: L.ByteString
+skeletonIdent = L.pack [0x66, 0x69, 0x73, 0x68, 0x65, 0x61, 0x64, 0x00]
+
 vorbisIdent :: L.ByteString
 vorbisIdent = L.pack [0x01, 0x76, 0x6f, 0x72, 0x62, 0x69, 0x73]
 
@@ -55,6 +59,7 @@ speexIdent = L.pack [0x53, 0x70, 0x65, 0x65, 0x78, 0x20, 0x20, 0x20]
 
 readCType :: L.ByteString -> Maybe OggType
 readCType d
+  | L.isPrefixOf skeletonIdent d = Just Skeleton
   | L.isPrefixOf vorbisIdent d = Just Vorbis
   | L.isPrefixOf speexIdent d = Just Speex
   | L.isPrefixOf theoraIdent d = Just Theora
@@ -68,6 +73,7 @@ readCType d
 -- readCType _ = Nothing
 
 parseType :: Maybe String -> Maybe OggType
+parseType (Just "skeleton") = Just Skeleton
 parseType (Just "vorbis") = Just Vorbis
 parseType (Just "speex") = Just Speex
 parseType (Just "theora") = Just Theora
@@ -92,6 +98,7 @@ instance Show OggTrack where
     "serialno " ++ show serialno ++ " (Unknown)"
 
 instance Show OggType where
+  show Skeleton = "Skeleton"
   show Vorbis = "Vorbis"
   show Speex  = "Speex"
   show Theora = "Theora"
