7d29fc50f8c36a120fa3a8863a11ea4fd629e347
Conrad Parker <conrad@metadecks.org>
2006-08-23 11:10:37 +0800

    add HUnit testing, integrated with cabal
    Add some basic testing of the ListMerge function, using HUnit. The build
    infrastructure to work with HUnit and Cabal is adapted from numericprelude.
    
    darcs-hash:20060823031037-4b390-c99f9222703a53fdaa08210d0a640f481bec9e23.gz

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,19 @@
 #!/usr/bin/env runhaskell
-import Distribution.Simple
-main = defaultMain
+
+import Distribution.Simple (defaultMainWithHooks, defaultUserHooks,
+                             UserHooks(..), Args)
+import           Distribution.PackageDescription (PackageDescription)
+import           Distribution.Simple.LocalBuildInfo (LocalBuildInfo)
+import qualified Distribution.Simple.LocalBuildInfo as LBI
+import           Distribution.Simple.Utils (rawSystemVerbose)
+import           Distribution.Compat.FilePath (joinPaths)
+import System.Exit(ExitCode(..))
+
+main :: IO ()
+main = defaultMainWithHooks (defaultUserHooks{runTests = tests})
+
+tests :: Args -> Bool -> LocalBuildInfo -> IO ExitCode
+tests args _ lbi =
+   let testCmd = foldl1 joinPaths [LBI.buildDir lbi, "ListMergeTest", "ListMergeTest"]
+   in  rawSystemVerbose 1 testCmd
+          ("+RTS" : "-M32m" : "-c30" : "-RTS" : args)
