bd54f9cf927125840e28030c80980e0d2037ea1e
Conrad Parker <conrad@metadecks.org>
2006-11-29 19:11:50 +0800

    fix up tests for newer Cabal version
    Not sure which version this changed in. I just upgraded from 1.1.3 in Debian to
    1.1.7 from darcs ...
    
    darcs-hash:20061129111150-4b390-ac65abe9e4374c667096cb85d7016c7285ae3a46.gz

--- Setup.hs
+++ Setup.hs
@@ -12,8 +12,12 @@ import System.Exit(ExitCode(..))
 main :: IO ()
 main = defaultMainWithHooks (defaultUserHooks{runTests = tests})
 
-tests :: Args -> Bool -> LocalBuildInfo -> IO ExitCode
-tests args _ lbi =
+-- The following two lines were for older cabal version
+-- tests :: Args -> Bool -> LocalBuildInfo -> IO ExitCode
+-- tests args _ lbi =
+
+tests :: Args -> Bool -> PackageDescription -> LocalBuildInfo -> IO ExitCode
+tests args _ _ lbi =
    let testCmd = foldl1 joinPaths [LBI.buildDir lbi, "ListMergeTest", "ListMergeTest"]
    in  rawSystemVerbose 1 testCmd
           ("+RTS" : "-M32m" : "-c30" : "-RTS" : args)
