25f6c4d365968834428d16cb6c23975e6ea189b6
Conrad Parker <conrad@metadecks.org>
2006-12-04 21:16:43 +0800

    make fromTwosComp. toTwosComp work on unsigned data; cleanup Ogg.Utils
    darcs-hash:20061204131643-4b390-d745b7f2b7b0c7e0530dd2ee75e98ff9b8d02092.gz

--- Codec/Container/Ogg/Utils.hs
+++ Codec/Container/Ogg/Utils.hs
@@ -13,13 +13,8 @@
 -----------------------------------------------------------------------------
 
 module Codec.Container.Ogg.Utils (
-   -- * Types and Constants
-   Octet,
-   msb,
-   -- * Octet Conversion Functions
-   fromTwosComp, toTwosComp,
-   toOctets, fromOctets,
-   i2osp
+   -- * Word8 Conversion Functions
+   fromTwosComp, toTwosComp
 	      ) where
 
 import Data.Word
@@ -34,70 +29,34 @@ toBase x =
    takeWhile (/=0) .
    iterate (flip div x)
 
--- | Take a number a convert it to base n as a list of octets.
 
-toOctets :: (Integral a, Integral b) => a -> b -> [Octet]
-toOctets n x = (toBase n . fromIntegral) x
-
--- | The basic type for encoding and decoding.
-
-type Octet = Word8
-
--- | The most significant bit of an 'Octet'.
+-- | The most significant bit of a Word8.
 
 msb :: Int
-msb = bitSize (undefined::Octet) - 1
+msb = bitSize (undefined::Word8) - 1
 
 -- | Take a list of octets (a number expressed in base n) and convert it
 --   to a number.
 
-fromOctets :: (Integral a, Integral b) => a -> [Octet] -> b
-fromOctets n x = 
+fromWord8s :: (Integral a, Integral b) => a -> [Word8] -> b
+fromWord8s n x = 
    fromIntegral $ 
    sum $ 
    zipWith (*) (powersOf n) (reverse (map fromIntegral x))
 
--- | Take the length of the required number of octets and convert the 
---   number to base 256 padding it out to the required length. If the
---   required length is less than the number of octets of the converted
---   number then return the converted number. NB this is different from
---   the standard <ftp://ftp.rsasecurity.com/pub/pkcs/pkcs-1/pkcs-1v2-1.pdf>
---   but mimics how replicate behaves.
+-- | Convert from twos complement, unsigned
 
-i2osp :: Integral a => Int -> a -> [Octet]
-i2osp l y = 
-   pad ++ z
-      where
-         pad = replicate (l - unPaddedLen) (0x00::Octet)
-	 z = toOctets 256 y
-	 unPaddedLen = length z
+fromTwosComp :: Integral a => [Word8] -> a
+fromTwoComp [] = 0
+fromTwosComp x = fromWord8s 256 x
 
--- | Convert from twos complement.
-
-fromTwosComp :: Integral a => [Octet] -> a
-fromTwosComp x =  conv x
-   where conv []       = 0
-         conv w@(x:xs) = if (testBit x msb)
-                            then neg w
-                            else pos w
-         neg w@(x:xs)  = let z=(clearBit x msb):xs in
-                            fromIntegral((fromOctets 256 z)-
-                                         (128*(256^((length w)-1))))
-         pos w         = fromIntegral(fromOctets 256 w)
-
-toTwosComp :: Integral a => a -> [Octet]
+-- | Convert to twos complement, unsigned
+toTwosComp :: Integral a => a -> [Word8]
 toTwosComp x
-   | x < 0     = reverse . plusOne . reverse . (map complement) $ u
+   | x < 0     = error "toTwosComp defined for unsigned only"
    | x == 0    = [0x00]
    | otherwise = u
    where z@(y:ys) = toBase 256 (abs x)
          u        = if testBit y msb
                        then 0x00:z
                        else z
-
-plusOne :: [Octet] -> [Octet]
-plusOne [] = [1]
-plusOne (x:xs) =
-   if x == 0xff
-      then 0x00:(plusOne xs)
-      else (x+1):xs
