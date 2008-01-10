50d4955891ed2b07a9d9ca0784d902677269816f
Conrad Parker <conrad@metadecks.org>
2008-01-10 10:05:51 +0800

    add Read instance for MessageHeaders
    darcs-hash:20080110020551-4b390-93fab8c4426e25fa1ea4515e67284098347a7a12.gz

--- Codec/Container/Ogg/MessageHeaders.hs
+++ Codec/Container/Ogg/MessageHeaders.hs
@@ -14,8 +14,9 @@ module Codec.Container.Ogg.MessageHeaders (
   mhAppend
 ) where
 
+import Data.Char
 import Data.List as List
-import Data.Map as Map
+import qualified Data.Map as Map
 
 ------------------------------------------------------------
 -- Data
@@ -48,7 +49,59 @@ mhInsert k v (MessageHeaders h) = MessageHeaders (Map.insert k [v] h)
 --
 
 mhAppend :: String -> String -> MessageHeaders -> MessageHeaders
-mhAppend k v (MessageHeaders h) = MessageHeaders (Map.insertWith (++) k [v] h)
+mhAppend k v mhdrs = mhAppends k [v] mhdrs
+
+mhAppends :: String -> [String] -> MessageHeaders -> MessageHeaders
+mhAppends k vs (MessageHeaders h) = MessageHeaders (Map.insertWith (++) k vs h)
+
+------------------------------------------------------------
+-- Read
+--
+
+instance Read MessageHeaders where
+  readsPrec _ = readMH mhEmpty
+
+readMH :: MessageHeaders -> ReadS MessageHeaders
+readMH mhdrs s = readMHlines mhdrs (lines s)
+
+readMHlines :: MessageHeaders -> [String] -> [(MessageHeaders, String)]
+readMHlines mhdrs [] = [(mhdrs, "")]
+
+-- Stop parsing at an empty line, ie. when the input contains CRLFCRLF
+readMHlines mhdrs ("":body) = [(mhdrs, unlines body)]
+
+readMHlines mhdrs (l:rest) = readMHlines mhdrs' rest
+  where
+    mhdrs' = mhAppends k vs mhdrs
+    (k, ':':vs'csv) = break (':' ==) l
+    vs' = split ',' vs'csv
+    vs = filter (not . null) $ map (filter isHTTPTokenChar) vs'
+
+split :: Char -> String -> [String]
+split = unfoldr . split'
+
+split' :: Char -> String -> Maybe (String, String)
+split' c l
+  | null l = Nothing
+  | otherwise = Just (h, drop 1 t)
+  where (h, t) = span (/=c) l
+
+------------------------------------------------------------
+-- Read helpers, based on grammar of RFC2616 sec. 2.1
+-- http://www.w3.org/Protocols/rfc2616/rfc2616-sec2.html#sec2
+--
+
+isHTTPCTL :: Char -> Bool
+isHTTPCTL c = (ord c <= 31) || (ord c) == 127
+
+isHTTPSeparator :: Char -> Bool
+isHTTPSeparator = flip elem ['(' , ')' , '<' , '>' , '@'
+                            , ',' , ';' , ':' , '\\' , '\"'
+                            , '/' , '[' , ']' , '?' , '='
+                            , '{' , '}' , ' ', '\t']
+
+isHTTPTokenChar :: Char -> Bool
+isHTTPTokenChar c = not (isHTTPCTL c || isHTTPSeparator c)
 
 ------------------------------------------------------------
 -- Show
@@ -56,7 +109,7 @@ mhAppend k v (MessageHeaders h) = MessageHeaders (Map.insertWith (++) k [v] h)
 
 instance Show MessageHeaders where
   show (MessageHeaders h) =
-    concat $ List.map serializeMH (assocs h)
+    concat $ List.map serializeMH (Map.assocs h)
     where
       serializeMH :: (String, [String]) -> String
       serializeMH (k, v) = k ++ ": " ++ (concat $ intersperse ", " v) ++ "\r\n"
