2dbac450f47f3daebd321c6c47867c3c7520eec0
Conrad Parker <conrad@metadecks.org>
2008-01-27 08:20:44 +0800

    implement sortHeaders', used in sort to ensure that all the header pages are in
    the correct order. This ensures that Theora BOS comes before Vorbis, and
    that Skeleton pages are in the right place.
    
    darcs-hash:20080127002044-4b390-bd85c873f3d29b0cb7d2037a413caf56c49feda1.gz

--- Codec/Container/Ogg/Sort.hs
+++ Codec/Container/Ogg/Sort.hs
@@ -12,10 +12,12 @@ module Codec.Container.Ogg.Sort (
   --, sortHeaders
 ) where
 
+import Codec.Container.Ogg.ContentType
 import Codec.Container.Ogg.List
 import Codec.Container.Ogg.Headers
 import Codec.Container.Ogg.Page
 import Codec.Container.Ogg.Serial
+import Codec.Container.Ogg.Track
 
 ------------------------------------------------------------
 -- Exposed functions
@@ -34,10 +36,27 @@ sort = sortHeaders . listMerge . demux
 -- | Ensure the header pages of each track are in the correct order
 --   relative to each other.
 sortHeaders :: [OggPage] -> [OggPage]
-sortHeaders = processHeaders sortHeaders'
+sortHeaders = processHeaders (sortHeaders' [] [] [] [] [])
 
-sortHeaders' :: [OggPage] -> [OggPage]
-sortHeaders' = id
+sortHeaders' :: [OggPage] -- skeleton bos
+             -> [OggPage] -- theora bos
+             -> [OggPage] -- other bos
+             -> [OggPage] -- other headers
+             -> [OggPage] -- skeleton eos
+             -> [OggPage] -- input
+             -> [OggPage] -- output
+sortHeaders' sb tb ob oh se [] = sb ++ tb ++ ob ++ oh ++ se
+sortHeaders' sb tb ob oh se (g:gs)
+  | contentTypeIs skeleton g = case (pageBOS g, pageEOS g) of
+    (True, False) -> sortHeaders' (sb++[g]) tb ob oh se gs
+    (False, True) -> sortHeaders' sb tb ob oh (se++[g]) gs
+    _             -> sortHeaders' sb tb ob (oh++[g]) se gs
+  | contentTypeIs theora g = case (pageBOS g) of
+    True          -> sortHeaders' sb (tb++[g]) ob oh se gs
+    False         -> sortHeaders' sb tb ob (oh++[g]) se gs
+  | otherwise = case (pageBOS g) of
+    True          -> sortHeaders' sb tb (ob++[g]) oh se gs
+    False         -> sortHeaders' sb tb ob (oh++[g]) se gs
 
 ------------------------------------------------------------
 -- mergeSkeleton
